11b2f6ae4d015a76bf72b0c8a0d7d1fdd19a8595
12345 <a123451@yandongyudeMBP.lan>
2018-03-29 00:52:53 +0800

    movescroll

---
+++ scroll.html/demo.js
@@ -0,0 +1,14 @@
+(function(doc, win){
+    var docE1 = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function(){
+            var clientWidth = docE1.clientWidth;
+            if(!clientWidth) return;
+            docE1.style.fontSize = 16 * (clientWidth / 375) + 'px';  
+            console.log(clientWidth)          
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt,recalc,false);
+    doc.addEventListener('DOMContentLoaded',recalc,false);
+})(document,window);
