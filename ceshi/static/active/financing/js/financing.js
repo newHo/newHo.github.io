c0c9ba41b06244d2e724ea51d46a23ffd19b5367
newHo <32625881+newHo@users.noreply.github.com>
2019-05-15 23:20:25 +0800

    shipei

---
+++ ceshi/static/active/financing/js/financing.js
@@ -0,0 +1,15 @@
+(function(doc, win) {
+    var docE1 = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docE1.clientWidth;
+            if (!clientWidth) return;
+            clientWidth >= 640 ? 640 : clientWidth;
+            docE1.style.fontSize = 100 * (clientWidth / 375) + 'px';
+
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
